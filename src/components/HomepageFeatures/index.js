3b70df242adb34cc90ff4f419f1531223207e7e5
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2024-11-10 15:27:59 +0530

    updated landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,22 +4,20 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Low Bandwidth',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The WebApp has been in such a way that it can perform smoothly in areas having low Bandwidth.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Diagnosis at Fingertips',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Scan for five intraoral diseases using our WebApp and get a diagnosis report.
       </>
     ),
   },
@@ -28,8 +26,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Built using React for quick loading and uses react router for creating pages.
       </>
     ),
   },
